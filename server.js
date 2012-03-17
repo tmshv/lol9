707d7b83c58891cc1ee181bf1c89bf159bb52461
Roman Timashev <roman@tmshv.ru>
2012-03-17 21:59:14 +0000

    server return mandelbrot;

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var express = require('express');
 var port = process.env.PORT;
 port = port ? port : 8720;
@@ -5,5 +6,14 @@ port = port ? port : 8720;
 var app = express.createServer();
 app.get('/', function(req, res){
     res.send('lol.cloudno.de');
+});
+app.get('/mandelbrot', function(req, res){
+    fs.readFile('mandelbrot.png', function(error, data){
+        if(error){
+            res.send(error.toString());   
+        }else{
+            res.send(data);
+        }
+    });
 });
 app.listen(port);
